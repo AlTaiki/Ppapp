bc4aa2507b946a3a3e730bef369eca8d68dc7f63
Hariss <taikiev.alex@gmail.com>
2021-12-09 16:19:44 +0300

    Update

---
+++ dbForLibraryApp/dbForLibraryApp/LibraryContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Sql;
+
+namespace dbForLibraryApp
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext() : base("DefaultConnection") { }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<AuthorAndBook> AuthorAndBooks { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookIssue> BookIssues { get; set; }
+    }
+}
