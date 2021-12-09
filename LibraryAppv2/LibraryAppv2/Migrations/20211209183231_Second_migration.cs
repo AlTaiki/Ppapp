daafc2d6633650ff561fc227c23529256be42e8b
Hariss <taikiev.alex@gmail.com>
2021-12-09 21:33:21 +0300

    Update migration

---
+++ LibraryAppv2/LibraryAppv2/Migrations/20211209183231_Second_migration.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryAppv2.Migrations
+{
+    public partial class Second_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId1",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId1",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId1",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Authors",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debtors_ClientId",
+                table: "Debtors",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Debtors_IssueId",
+                table: "Debtors",
+                column: "IssueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreId",
+                table: "Books",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_PublisherId",
+                table: "Books",
+                column: "PublisherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookIssues_BookId",
+                table: "BookIssues",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookIssues_Books_BookId",
+                table: "BookIssues",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Genre_GenreId",
+                table: "Books",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books",
+                column: "PublisherId",
+                principalTable: "Publishers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Debtors_BookIssues_IssueId",
+                table: "Debtors",
+                column: "IssueId",
+                principalTable: "BookIssues",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Debtors_Clients_ClientId",
+                table: "Debtors",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookIssues_Books_BookId",
+                table: "BookIssues");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Authors_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Genre_GenreId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Publishers_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Debtors_BookIssues_IssueId",
+                table: "Debtors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Debtors_Clients_ClientId",
+                table: "Debtors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Debtors_ClientId",
+                table: "Debtors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Debtors_IssueId",
+                table: "Debtors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_GenreId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_PublisherId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookIssues_BookId",
+                table: "BookIssues");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorId1",
+                table: "Books",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Authors",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId1",
+                table: "Books",
+                column: "AuthorId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Authors_AuthorId1",
+                table: "Books",
+                column: "AuthorId1",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
