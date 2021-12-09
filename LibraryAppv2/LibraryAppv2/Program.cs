4b62448314fe08bab617955fee33cb9d713c167b
Hariss <taikiev.alex@gmail.com>
2021-12-09 21:18:06 +0300

    App v0.1

--- LibraryAppv2/LibraryAppv2/Program.cs
+++ LibraryAppv2/LibraryAppv2/Program.cs
@@ -6,7 +6,7 @@ namespace LibraryAppv2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LibraryContext db = new LibraryContext();
         }
     }
 }
